c01b6d64c0416275d953d27046e4ce77e1c3c520
evanmanister <117755194+evanmanister@users.noreply.github.com>
2022-11-08 16:02:21 +0000

    Delete Evan Manister/Visual Studio 2022 directory

--- Evan Manister/Visual Studio 2022/Python Scripts/Python 3.6 (32-bit)/repos/PythonApplication3/PythonApplication3/PythonApplication3.py
+++
@@ -1,7 +0,0 @@
-name = input("please enter your name: ")
-
-while len(name) == 0:
-    print("You did not enter a name.")
-    name = input("Please enter your name: ")
-
-print("Thank you " + " for entering your name!")
