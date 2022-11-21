8c22b5c3e8dd76b91e24295abd872cf5098f6ae6
haithm4 <117755644+haithm4@users.noreply.github.com>
2022-11-21 22:08:54 +0000

    SCOOP INSTALL AN-CLI

--- Haitham Mohammed/Main/Main/Main.py
+++ Haitham Mohammed/Main/Main/Main.py
@@ -1,3 +1,80 @@
+<<<<<<< HEAD
+from characters import *
+
+def generate_goblin_list():
+    num_of_enemies = 0
+    num_of_enemies = int(input(
+        "Enter how many goblins you would like to battle: "))
+     
+    for i in range(num_of_enemies):
+        goblin_list.append(Goblin())
+
+def generate_hobgoblin_list():
+    num_of_enemies = 0
+    num_of_enemies = int(input(
+        "Enter how many hobgoblins you would like to battle: "))
+     
+    for i in range(num_of_enemies):
+        hobgoblin_list.append(Hobgoblin())
+
+class_input = str(input("Choose a player class\n Hunter\n Titan\n Warlock\n : "))
+if class_input == "Hunter" or class_input == "hunter":    
+    player_1 = Hunter()
+    print("You have chosen the Hunter Class ")
+    player_1.weapon()
+    print(vars(player_1))
+    generate_goblin_list()
+    for j in range(len(goblin_list)):
+        # checks len of list (matching the inputed value)
+        print(vars(goblin_list[j]))
+    generate_hobgoblin_list()
+    story(Hobgoblin)
+
+elif class_input == "Titan" or class_input == "titan":
+    player_1 = Titan()
+    print("You have chosen the Titan Class ")
+    player_1.weapon()
+    print(vars(player_1))
+    generate_goblin_list()
+    for j in range(len(goblin_list)):
+        # checks len of list (matching the inputed value)
+        print(vars(goblin_list[j]))
+    generate_hobgoblin_list()
+    for j in range(len(hobgoblin_list)):
+        # checks len of list (matching the inputed value)
+        print(vars(hobgoblin_list[j]))
+    story(Hobgoblin)
+elif class_input == "Warlock" or class_input == "warlock":
+    player_1 = Warlock()
+    print("You have chosen the Warlock Class ")
+    player_1.weapon()
+    print(vars(player_1))
+    generate_goblin_list()
+    for j in range(len(goblin_list)):
+        # checks len of list (matching the inputed value)
+        print(vars(goblin_list[j]))
+    generate_hobgoblin_list()
+    for j in range(len(hobgoblin_list)):
+        # checks len of list (matching the inputed value)
+        print(vars(hobgoblin_list[j]))
+    story(Hobgoblin)
+ 
+else:
+    print("You obviously did not get sent to school, learn how to spell and try again! ")
+    class_input = input("Choose a player class\n Hunter\n Titan\n Warlock\n:")
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 from characters import *
 
 class_input = str(input("Choose a player class\n Hunter\n Titan\n Warlock\n : "))
@@ -35,3 +112,4 @@ for j in range(len(enemy_list)):
     # checks len of list (matching the inputed value)
     print(vars(enemy_list[j]))
 
+>>>>>>> 98e7e1f627f822d717cb5a658057910330f30225
--- Haitham Mohammed/Main/Main/characters.py
+++ Haitham Mohammed/Main/Main/characters.py
@@ -1,3 +1,120 @@
+<<<<<<< HEAD
+
+from random import  randint
+goblin_list = []
+hobgoblin_list = []
+class Goblin:
+    def __init__(self):
+        self.Name = "Goblin"
+        self.Level = 1
+        self.Attack = randint(40,60)
+        self.Health = randint(10,60)
+        self.Defence = randint(30,50)
+ 
+class Hobgoblin(Goblin):
+    def __init__(self):
+        Goblin.__init__(self)
+        self.Name = "Hobgoblin"
+        self.Health = (self.Health * 1.25)
+        self.Attack = (self.Attack * 1.25)
+        self.Defence = (self.Defence * 1.25)
+
+def story(Hobgoblin):
+        inp_3 = "You see a large treasure chest and no one is around!, \
+do you want to open it yes or no(y/n): "
+        input_3 = input(inp_3)
+        if input_3 == "y":
+            
+            print("You have been trapped and you died !")
+            quit()  
+        else:
+            print("Smart choice, you avoided a coin trap. You survived!") 
+            inp_4 = "A hobgoblin attacks you. Do you want to block its attack\
+ ,attack it or run away: "
+            input_4 =input(inp_4)
+            if input_4 == "Attack" or input_4 == "attack":
+                
+                print(f"You dealt 10 damage to the hobgoblin, its health is now {Hobgoblin.Health} ")
+class Titan :
+
+    def __init__(self):
+        self.Class = "Titan"
+        self.Level = 1
+        self.Attack = randint(80,200)
+        self.Health = randint(65,100)
+        self.Defence = randint(80,100)
+    
+    def weapon(self):
+      rand = randint(1,5)
+      if rand <= 5:
+            self.weapon = "LA CHANCLA"
+            print("You have equipped LA CHANCLA")
+      else:
+            self.weapon = "DA CROCS"
+            print("You have eqipped DA CROCS ")
+    
+    
+
+class Warlock :
+    def __init__(self):
+        self.Class = "Warlock"
+        self.Level = 1
+        self.Attack = randint(75,100)
+        self.Health = randint(50,100)
+        self.Defence = randint(70,100)
+    def weapon(self):
+       rand = randint(1,5)
+       if rand <= 5:
+            self.weapon = "LOS FUTBOL"
+            print("You have equipped LOS FUTBOL")
+       else:
+            self.weapon = "DA  BANANANA"
+            print("You have eqipped DA BANANANA ")
+
+
+ 
+
+class Hunter :
+    def __init__(self):
+        self.Class = "Hunter"
+        self.Level = 1
+        self.Attack = randint(80,100)
+        self.Health = randint(60,100)
+        self.Defence = randint(60,100)
+
+
+
+    def weapon(self):
+        rand = randint(1,5)
+        if rand <= 5:
+            self.weapon = "LOS SANDALIAS"
+            print("You have equipped LOS SANDALIAS")
+        else:
+            self.weapon = "DA ZUCC"
+            print("You have eqipped DA ZUCC ")
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 
 from random import  randint
 enemy_list = []
@@ -140,3 +257,4 @@ class Hunter :
 
 
 
+>>>>>>> 98e7e1f627f822d717cb5a658057910330f30225
