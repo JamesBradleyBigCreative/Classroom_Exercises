f08d56837decd725b3d28c6a7bbdcefb3381d0a3
evanmanister <117755194+evanmanister@users.noreply.github.com>
2022-11-23 13:40:15 +0000

    Add files via upload

---
+++ Evan Manister/GAME 2/gamefunctions.py
@@ -0,0 +1,12 @@
+﻿from random import randint
+from module1 import *
+def dice_roll():
+    dice_result = randint(1,6)
+
+    return dice_result
+
+print("You need to roll a dice")
+print("Please enter 'ok' when you want to roll a dice")
+
+input()
+
